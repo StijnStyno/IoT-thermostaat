e4f29ab27426ebca5c51ab471ff7736bd06b1061
StijnStyno <92586070+StijnStyno@users.noreply.github.com>
2022-03-16 17:13:34 +0100

    Add files via upload

---
+++ IoTthermometer.ino
@@ -0,0 +1,42 @@
+#include <DHT.h>
+
+#define DHTPIN 4
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN,DHTTYPE);
+
+char auth[] = BLYNK_AUTH_TOKEN;
+char ssid[] = "Wifi_Bjorn_Boven";
+char pass[] = "Patayastraat14";
+
+
+void setup() 
+{
+  pinMode(2,INPUT_PULLUP);
+  pinMode(5,OUTPUT);
+  Serial.begin(115200);
+  dht.begin();
+}
+
+void loop()
+{
+  delay(2000);
+  float p = analogRead(2);
+  float t2 = map(p,0,4095,0,30);
+  float t = dht.readTemperature();
+  if (isnan(t)) 
+  {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+  if (t2 > t)
+  {
+    digitalWrite(5,HIGH);
+  }
+  else
+    digitalWrite(5,LOW);
+  Serial.print("temperature: ");
+  Serial.println(t);
+  Serial.print("Chosen temperature: ");
+  Serial.println(t2);
+}
